0fd049b07deae2d8ae845a9d55ea623c069143cf
Abhishek Sharma <abhishek79009@gmail.com>
2023-11-05 02:02:05 +0530

    production included

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use(express.json());
 const cors = require('cors');
 
 let origin = 'http://localhost:3000';
+
+if (process.env.NODE_ENV?.trim() === "production") {
+  origin = process.env.CLIENT_URL
+}
 app.use(cors({
   credentials: true,
   origin
